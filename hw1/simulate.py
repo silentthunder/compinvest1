10598c4ad7c444caf1fcfc4a2eef35b31b47a198
wojciech.kepper <wojciech.kepper@gamesys.co.uk>
2013-03-17 20:47:55 +0000

    hw1 part 1 done

--- hw1/simulate.py
+++ hw1/simulate.py
@@ -9,17 +9,19 @@ import QSTK.qstkutil.DataAccess as da
 import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from duplicity.globals import volsize
+from numpy.ma.core import sqrt
 
-def simulate(dt_start, dt_end, ls_symbols, allocations):
+def simulate(dt_start, dt_end, ls_symbols, na_allocations):
     print 'Simulate'
     # We need closing prices so the timestamp should be hours=16.
     dt_timeofday = dt.timedelta(hours=16)
 
     # Get a list of trading days between the start and the end.
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
-
     # Creating an object of the dataaccess class with Yahoo as the source.
-    c_dataobj = da.DataAccess('Yahoo')
+    c_dataobj = da.DataAccess('Yahoo',cachestalltime=0)
 
     # Keys to be read from the data, it is good to read everything in one go.
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
@@ -32,22 +34,41 @@ def simulate(dt_start, dt_end, ls_symbols, allocations):
     # Getting the numpy ndarray of close prices.
     na_price = d_data['close'].values
     
-     # Normalizing the prices to start at 1 and see relative returns
+    # Normalizing the prices to start at 1 and see relative returns
     na_normalized_price = na_price / na_price[0, :]
     
-    # portfolio valu
-    na_portf_norm_price = na_normalized_price * allocations
+    # portfolio value
+    na_cum_daily_portf_value = np.dot(na_normalized_price, na_allocations)
+    #print na_cum_daily_portf_value.size #should be 252
+    n_trading_days =  na_cum_daily_portf_value.size
     
+    # daily returns
+    na_daily_rets = na_cum_daily_portf_value.copy()
+    tsu.returnize0(na_daily_rets)
     
+    #volatility
+    na_vol = np.std(na_daily_rets)
     
+    #sharp
+    na_sharp = np.sqrt(252)*np.mean(na_daily_rets)/na_vol
+    
+    return (na_vol, na_daily_rets, na_sharp, na_cum_daily_portf_value[na_cum_daily_portf_value.size-1])
 
 def main():
     print 'Main'
-    ls_symbols = ["AAPL", "GLD", "GOOG", "$SPX", "XOM"]
-    dt_start = dt.datetime(2006, 1, 1)
+    ls_symbols = ['AXP', 'HPQ', 'IBM', 'HNZ']
+    dt_start = dt.datetime(2010, 1, 1)
     dt_end = dt.datetime(2010, 12, 31)
-    allocations = [0.1, 0.1, 0.5, 0.3]
-    simulate()
+    na_allocations = np.array([0.0, 0.0, 0.0, 1.0])
+    vol, daily_ret, sharpe, cum_ret = simulate(dt_start, dt_end, ls_symbols, na_allocations)
+    print 'Sharpe ratio '
+    print sharpe
+    print 'Volatility'
+    print vol
+    print 'Avg daily ret'
+    print np.average(daily_ret)
+    print 'Cumulative return'
+    print cum_ret
     
 if __name__ == '__main__':
     main()
