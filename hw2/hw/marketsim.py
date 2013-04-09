d53f564e86c6b55ca666218a66358b4c7924e3fc
Wojciech Kepper <wojciech.kepper@gmail.com>
2013-04-10 00:05:26 +0100

    market simulator part 1 complete

--- hw2/hw/marketsim.py
+++ hw2/hw/marketsim.py
@@ -2,11 +2,12 @@
 import sys
 import numpy as np
 import datetime as dt
+import pandas as pd
 
 #QSTK imports
 import QSTK.qstkutil.DataAccess as da
 import QSTK.qstkutil.qsdateutil as du
-from test.test_iterlen import len
+from posixpath import samefile
 
 def main(argv):
     print 'Number of arguments:', len(argv), 'arguments.'
@@ -28,7 +29,7 @@ def main(argv):
     #scan trades for symbols and dates - built a list of symbols and a date range
     ls_symbols = []
     ls_dates = []
-        
+    
     for record in na_trades:
         ls_symbols.append(record[3])        
         aDate = dt.datetime(record[0], record[1], record[2])
@@ -39,9 +40,9 @@ def main(argv):
     na_trades_norm = sorted(na_trades_norm,key=lambda trade: trade[0])
     #print 'norm trades sorted ',na_trades_norm
     set_symbols = set(ls_symbols)
-    ls_dates = sorted(ls_dates)
-    dt_start = ls_dates[0]
-    dt_end = ls_dates[len(ls_dates)-1]
+    ls_dates_sorted = sorted(ls_dates)
+    dt_start = ls_dates_sorted[0]
+    dt_end = ls_dates_sorted[len(ls_dates)-1]
     
     print 'start date:',dt_start
     print 'end_date:', dt_end
@@ -55,7 +56,7 @@ def main(argv):
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     ldf_data = c_dataobj.get_data(ldt_timestamps, set_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
-    df_actual_close = d_data['actual_close'].copy()
+    df_actual_close = d_data['close'].copy()
     #print(df_actual_close)
     #print(df_actual_close.index)
 
@@ -73,24 +74,59 @@ def main(argv):
             currentCash -= (nShares*price)
         else:
             currentCash += (nShares*price)
+        print currentCash
         ls_cash.append(currentCash)
         
     print 'ls_cash ', ls_cash
     
     #scan trades to create ownership array & value
-    print 'all symbols:',set_symbols 
-    np_ownership = np.zeros((len(na_trades_norm),len(set_symbols)))
-    print np_ownership
+    #print 'all symbols:',set_symbols
+    df_ownership = pd.DataFrame(np.zeros((len(na_trades_norm), len(set_symbols))), columns=set_symbols)
+    i = 0
+    ls_value = []
     for trade in na_trades_norm:
         dt_date = trade[0]
         symbol = trade[1]
         txType = trade[2]
         nShares = trade[3]
-        if (symbol == 'Sell'):
+        if (txType == 'Sell'):
             nShares = -nShares
-        np_ownership[]
+        if i == 0: 
+            df_ownership.ix[i].ix[symbol] = nShares
+        else:
+            for stockSymbol in set_symbols:
+                #copy the previous state
+                df_ownership.ix[i].ix[stockSymbol] = df_ownership.ix[i-1].ix[stockSymbol]
+            df_ownership.ix[i].ix[symbol] += nShares
+        
+        assetsValueAfterTx = 0
+        for stockSymbol in set_symbols:
+            stockPrice = df_actual_close[stockSymbol].ix[dt_date+dt_timeofday]
+            assetsValueAfterTx += stockPrice * df_ownership.ix[i].ix[stockSymbol] 
+        
+        #df_ownership.ix[i].ix['Value'] = assetsValueAfterTx
+        ls_value.append(assetsValueAfterTx)
+        i += 1
+    print df_ownership
     
     #scan cash and value to create total fund value
+    np_total_fund_value = np.array(ls_cash) + np.array(ls_value)
+    print np_total_fund_value
+    
+    #generate result file
+    fout = open(valuesFilename, 'w')
+    print ls_dates_sorted
+    i = 0
+    for trade in na_trades_norm:
+        dt_date = trade[0]
+        
+        line ='%d, %d, %d, %f\n' % (dt_date.year,dt_date.month,dt_date.day,np_total_fund_value[i]) 
+        print line
+        fout.write(line)
+        i += 1
+        
+    #fout.write('')
+    fout.close()
     
 if __name__ == "__main__":
    main(sys.argv[1:])
